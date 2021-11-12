65ceeac21110acfdc26c7c23249b2d342e1f5fac
Gary Peng <22gapeng22@gmail.com>
2021-11-12 12:49:10 -0500

    11/12/2021

--- ChessEngine/Engine.h
+++ ChessEngine/Engine.h
@@ -10,8 +10,8 @@ class Engine {
     chrono::time_point<chrono::steady_clock> start;
     int seconds;
     void makeMove(State& state, tuple<int, int, int, int, int> move);
-    pair<double, int> negamax(State& state, int depths, double maximumOptimalEvaluation);
     tuple<int, int, int, int, int, double, int> negamax(State& state, int depths);
+    pair<double, int> negamax(State& state, int depths, double maximumOptimalEvaluation);
 public:
     tuple<int, int, int, int, int, double, int, int> getOptimalMove(string FEN, int depths);
 };
