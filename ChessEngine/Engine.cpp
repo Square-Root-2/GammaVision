5c5023731b14f3bfecd9f7defb4ea66985df3755
Gary Peng <22gapeng22@gmail.com>
2021-11-12 12:22:29 -0500

    11/12/2021

--- ChessEngine/Engine.cpp
+++ ChessEngine/Engine.cpp
@@ -3,49 +3,6 @@
 #include "MoveGenerator.h"
 #include <random>
 
-tuple<int, int, int, int, int, double, int> Engine::getOptimalMove(State& state, int depths, double maximumOptimalEvaluation) {
-    if (chrono::duration_cast<chrono::seconds>(chrono::steady_clock::now() - start).count() >= seconds)
-        return tuple<int, int, int, int, int, double, int>(-2, -2, -2, -2, -2, 0, INT32_MAX);
-    vector<tuple<int, int, int, int, int>> moves = MoveGenerator::getMoves(state);
-    if (moves.empty())
-        return tuple<int, int, int, int, int, double, int>(-1, -1, -1, -1, -1, state.isActiveColorInCheck() ? -INFINITY : 0, 0);
-    if (depths == 0)
-        return tuple<int, int, int, int, int, double, int>(-1, -1, -1, -1, -1, Evaluator::getEvaluation(state), INT32_MAX);
-    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-    shuffle(moves.begin(), moves.end(), rng);
-    tuple<int, int, int, int, int> optimalMove;
-    double optimalEvaluation = -INFINITY;
-    int minimumMoves = INT32_MAX;
-    int maximumMoves = 0;
-    for (int i = 0; i < moves.size(); i++) {
-        tuple<string, bool, int, int> hashCode = state.getHashCode();
-        makeMove(state, moves[i]);
-        tuple<int, int, int, int, int, double, int> opponentOptimalMove = getOptimalMove(state, depths - 1, -optimalEvaluation);
-        if (get<0>(opponentOptimalMove) == -2)
-            return tuple<int, int, int, int, int, double, int>(-2, -2, -2, -2, -2, 0, INT32_MAX);
-        get<5>(opponentOptimalMove) = -get<5>(opponentOptimalMove);
-        if (get<5>(opponentOptimalMove) > optimalEvaluation) {
-            optimalMove = moves[i];
-            optimalEvaluation = get<5>(opponentOptimalMove);
-            if (get<5>(opponentOptimalMove) == -INFINITY)
-                maximumMoves = get<6>(opponentOptimalMove) + 1;
-            else if (get<5>(opponentOptimalMove) == INFINITY)
-                minimumMoves = get<6>(opponentOptimalMove) + 1;
-        }
-        else if (get<5>(opponentOptimalMove) == optimalEvaluation && get<5>(opponentOptimalMove) == -INFINITY && get<6>(opponentOptimalMove) + 1 > maximumMoves) {
-            optimalMove = moves[i];
-            maximumMoves = get<6>(opponentOptimalMove) + 1;
-        }
-        else if (get<5>(opponentOptimalMove) == optimalEvaluation && get<5>(opponentOptimalMove) == INFINITY && get<6>(opponentOptimalMove) + 1 < minimumMoves) {
-            optimalMove = moves[i];
-            minimumMoves = get<6>(opponentOptimalMove) + 1;
-        }
-        state.setHashCode(hashCode);
-        if (optimalEvaluation > maximumOptimalEvaluation || abs(optimalEvaluation) != INFINITY && optimalEvaluation == maximumOptimalEvaluation)
-            return tuple<int, int, int, int, int, double, int>(get<0>(optimalMove), get<1>(optimalMove), get<2>(optimalMove), get<3>(optimalMove), get<4>(optimalMove), optimalEvaluation, INT32_MAX);
-    }
-    return tuple<int, int, int, int, int, double, int>(get<0>(optimalMove), get<1>(optimalMove), get<2>(optimalMove), get<3>(optimalMove), get<4>(optimalMove), optimalEvaluation, optimalEvaluation == -INFINITY ? maximumMoves : optimalEvaluation == INFINITY ? minimumMoves : INT32_MAX);
-}
 void Engine::makeMove(State& state, tuple<int, int, int, int, int> move) {
     state.setPiece(get<2>(move), get<3>(move), state.getPiece(get<0>(move), get<1>(move)));
     state.setPiece(get<0>(move), get<1>(move), '.');
@@ -80,15 +37,88 @@ void Engine::makeMove(State& state, tuple<int, int, int, int, int> move) {
         state.setPossibleEnPassantTargetColumn(-1);
     state.toggleActiveColor();
 }
+tuple<int, int, int, int, int, double, int> Engine::negamax(State& state, int depths) {
+    if (chrono::duration_cast<chrono::seconds>(chrono::steady_clock::now() - start).count() >= seconds)
+        return tuple<int, int, int, int, int, double, int>(-1, 0, 0, 0, 0, 0, 0);
+    vector<tuple<int, int, int, int, int>> moves = MoveGenerator::getMoves(state);
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+    shuffle(moves.begin(), moves.end(), rng);
+    tuple<int, int, int, int, int> optimalMove;
+    double optimalEvaluation = -INFINITY;
+    int minimumMoves = INT32_MAX;
+    int maximumMoves = 0;
+    tuple<string, bool, int, int> hashCode = state.getHashCode();
+    for (int i = 0; i < moves.size(); i++) {
+        makeMove(state, moves[i]);
+        pair<double, int> opponentOptimalEvaluation = negamax(state, depths - 1, INFINITY);
+        if (opponentOptimalEvaluation.second == -1)
+            return tuple<int, int, int, int, int, double, int>(-1, 0, 0, 0, 0, 0, 0);
+        if (-opponentOptimalEvaluation.first > optimalEvaluation) {
+            optimalMove = moves[i];
+            optimalEvaluation = -opponentOptimalEvaluation.first;
+            if (-opponentOptimalEvaluation.first == -INFINITY)
+                maximumMoves = opponentOptimalEvaluation.second + 1;
+            else if (-opponentOptimalEvaluation.first == INFINITY)
+                minimumMoves = opponentOptimalEvaluation.second + 1;
+        }
+        else if (-opponentOptimalEvaluation.first == optimalEvaluation && -opponentOptimalEvaluation.first == -INFINITY && opponentOptimalEvaluation.second + 1 > maximumMoves) {
+            optimalMove = moves[i];
+            maximumMoves = opponentOptimalEvaluation.second + 1;
+        }
+        else if (-opponentOptimalEvaluation.first == optimalEvaluation && -opponentOptimalEvaluation.first == INFINITY && opponentOptimalEvaluation.second + 1 < minimumMoves) {
+            optimalMove = moves[i];
+            minimumMoves = opponentOptimalEvaluation.second + 1;
+        }
+        state.setHashCode(hashCode);
+    }
+    return tuple<int, int, int, int, int, double, int>(get<0>(optimalMove), get<1>(optimalMove), get<2>(optimalMove), get<3>(optimalMove), get<4>(optimalMove), optimalEvaluation, optimalEvaluation == -INFINITY ? maximumMoves : optimalEvaluation == INFINITY ? minimumMoves : INT32_MAX);
+}
+pair<double, int> Engine::negamax(State& state, int depths, double maximumOptimalEvaluation) {
+    if (chrono::duration_cast<chrono::seconds>(chrono::steady_clock::now() - start).count() >= seconds)
+        return pair<double, int>(0, -1);
+    vector<tuple<int, int, int, int, int>> moves = MoveGenerator::getMoves(state);
+    if (moves.empty())
+        return pair<double, int>(state.isActiveColorInCheck() ? -INFINITY : 0, 0);
+    if (depths == 0)
+        return pair<double, int>(Evaluator::getEvaluation(state), INT32_MAX);
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+    shuffle(moves.begin(), moves.end(), rng);
+    double optimalEvaluation = -INFINITY;
+    int minimumMoves = INT32_MAX;
+    int maximumMoves = 0;
+    tuple<string, bool, int, int> hashCode = state.getHashCode();
+    for (int i = 0; i < moves.size(); i++) {
+        makeMove(state, moves[i]);
+        pair<double, int> opponentOptimalEvaluation = negamax(state, depths - 1, INFINITY);
+        if (opponentOptimalEvaluation.second == -1)
+            return pair<double, int>(0, -1);
+        if (-opponentOptimalEvaluation.first > optimalEvaluation) {
+            optimalEvaluation = -opponentOptimalEvaluation.first;
+            if (-opponentOptimalEvaluation.first == -INFINITY)
+                maximumMoves = opponentOptimalEvaluation.second + 1;
+            else if (-opponentOptimalEvaluation.first == INFINITY)
+                minimumMoves = opponentOptimalEvaluation.second + 1;
+        }
+        else if (-opponentOptimalEvaluation.first == optimalEvaluation && -opponentOptimalEvaluation.first == -INFINITY && opponentOptimalEvaluation.second + 1 > maximumMoves)
+            maximumMoves = opponentOptimalEvaluation.second + 1;
+        else if (-opponentOptimalEvaluation.first == optimalEvaluation && -opponentOptimalEvaluation.first == INFINITY && opponentOptimalEvaluation.second + 1 < minimumMoves)
+            minimumMoves = opponentOptimalEvaluation.second + 1;
+        state.setHashCode(hashCode);
+        // alpha-beta pruning
+        if (optimalEvaluation > maximumOptimalEvaluation || abs(optimalEvaluation) != INFINITY && optimalEvaluation == maximumOptimalEvaluation)
+            return pair<double, int>(optimalEvaluation, INT32_MAX);
+    }
+    return pair<double, int>(optimalEvaluation, optimalEvaluation == -INFINITY ? maximumMoves : optimalEvaluation == INFINITY ? minimumMoves : INT32_MAX);
+}
 tuple<int, int, int, int, int, double, int, int> Engine::getOptimalMove(string FEN, int seconds) {
-    tuple<int, int, int, int, int, double, int, int> optimalMove;
-    start = chrono::steady_clock::now();
+    tuple<int, int, int, int, int, double, int> optimalMove;
     this->seconds = seconds;
+    start = chrono::steady_clock::now();
     for (int depths = 1; ; depths++) {
         State state(FEN);
-        tuple<int, int, int, int, int, double, int> move = getOptimalMove(state, depths, INFINITY);
-        if (get<0>(move) == -2)
-            return optimalMove;
-        optimalMove = tuple<int, int, int, int, int, double, int, int>(get<0>(move), get<1>(move), get<2>(move), get<3>(move), get<4>(move), get<5>(move), get<6>(move), depths);
+        tuple<int, int, int, int, int, double, int> move = negamax(state, depths);
+        if (get<0>(move) == -1)
+            return tuple<int, int, int, int, int, double, int, int>(get<0>(optimalMove), get<1>(optimalMove), get<2>(optimalMove), get<3>(optimalMove), get<4>(optimalMove), get<5>(optimalMove), get<6>(optimalMove), depths);
+        optimalMove = move;
     }
 }
--- ChessEngine/Engine.h
+++ ChessEngine/Engine.h
@@ -8,8 +8,9 @@ using namespace std;
 class Engine {
     chrono::time_point<chrono::steady_clock> start;
     int seconds;
-    tuple<int, int, int, int, int, double, int> getOptimalMove(State& state, int depths, double maximumOptimalEvaluation);
     void makeMove(State& state, tuple<int, int, int, int, int> move);
+    pair<double, int> negamax(State& state, int depths, double maximumOptimalEvaluation);
+    tuple<int, int, int, int, int, double, int> negamax(State& state, int depths);
 public:
     tuple<int, int, int, int, int, double, int, int> getOptimalMove(string FEN, int depths);
 };
