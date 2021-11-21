0cab355e1dd9dec5e35043bf678271e02673d7a2
Gary Peng <22gapeng22@gmail.com>
2021-11-21 14:46:59 -0500

    11/21/2021

--- ChessEngine/Engine.cpp
+++ ChessEngine/Engine.cpp
@@ -17,7 +17,7 @@ pair<Move, double> Engine::negamax(State& state, int depth) {
     tuple<string, bool, int, int> hashCode = state.getHashCode();
     for (int i = 0; i < moves.size(); i++) {
         state.makeMove(moves[i]);
-        double evaluation = -negamax(state, 1, depth, -INFINITY, -alpha, true);
+        double evaluation = -negamax(state, 1, depth, -INFINITY, -alpha);
         if (evaluation == Evaluator::getMaximumEvaluation() + getMaximumNegamaxDepth() + getMaximumQuiescenceDepth() + 2)
             return pair<Move, double>(Move(0, 0, 0, 0, MoveType::TIMEOUT, ' ', ' '), 0);
         state.setHashCode(hashCode);
@@ -29,7 +29,7 @@ pair<Move, double> Engine::negamax(State& state, int depth) {
     killerMoves[1].clear();
     return pair<Move, double>(optimalMove, alpha);
 }
-double Engine::negamax(State& state, int currentDepth, int depth, double alpha, double beta, bool nullOk) {
+double Engine::negamax(State& state, int currentDepth, int depth, double alpha, double beta) {
     if (chrono::duration_cast<chrono::seconds>(chrono::steady_clock::now() - start).count() >= seconds)
         return -(Evaluator::getMaximumEvaluation() + getMaximumNegamaxDepth() + getMaximumQuiescenceDepth() + 2);
     vector<Move> moves = MoveGenerator::getMoves(state);
@@ -37,17 +37,14 @@ double Engine::negamax(State& state, int currentDepth, int depth, double alpha, 
         return state.isActiveColorInCheck() ? -(Evaluator::getMaximumEvaluation() + getMaximumNegamaxDepth() + getMaximumQuiescenceDepth() + 1 - currentDepth) : 0;
     if (currentDepth >= depth)
         return quiescenceSearch(state, currentDepth, alpha, beta);
-    if (!state.isActiveColorInCheck() && nullOk) {
+    if (!state.isActiveColorInCheck()) {
         state.toggleActiveColor();
-        double evaluation = -negamax(state, currentDepth + 1, depth - (depth > 6 ? MAX_R : MIN_R), -beta, -beta + 1, false);
+        double evaluation = -negamax(state, currentDepth + 1, depth - R, -beta, -beta + 1);
         if (evaluation == Evaluator::getMaximumEvaluation() + getMaximumNegamaxDepth() + getMaximumQuiescenceDepth() + 2)
             return -(Evaluator::getMaximumEvaluation() + getMaximumNegamaxDepth() + getMaximumQuiescenceDepth() + 2);
         state.toggleActiveColor();
-        if (evaluation >= beta) {
-            depth -= DR;
-            if (currentDepth >= depth)
-                return quiescenceSearch(state, currentDepth, alpha, beta);
-        }
+        if (evaluation >= beta)
+            return evaluation;
     }
     mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
     shuffle(moves.begin(), moves.end(), rng);
@@ -56,7 +53,7 @@ double Engine::negamax(State& state, int currentDepth, int depth, double alpha, 
     tuple<string, bool, int, int> hashCode = state.getHashCode();
     for (int i = 0; i < moves.size(); i++) {
         state.makeMove(moves[i]);
-        double evaluation = -negamax(state, currentDepth + 1, depth, -beta, -alpha, true);
+        double evaluation = -negamax(state, currentDepth + 1, depth, -beta, -alpha);
         if (evaluation == Evaluator::getMaximumEvaluation() + getMaximumNegamaxDepth() + getMaximumQuiescenceDepth() + 2)
             return -(Evaluator::getMaximumEvaluation() + getMaximumNegamaxDepth() + getMaximumQuiescenceDepth() + 2);
         state.setHashCode(hashCode);
--- ChessEngine/Engine.h
+++ ChessEngine/Engine.h
@@ -10,12 +10,12 @@
 using namespace std;
 
 class Engine {
-    static const int MAXIMUM_NEGAMAX_DEPTH = 14, MAXIMUM_QUIESCENCE_DEPTH = 30, MIN_R = 3, MAX_R = 4, DR = 4;
+    static const int MAXIMUM_NEGAMAX_DEPTH = 14, MAXIMUM_QUIESCENCE_DEPTH = 30, R = 3;
     chrono::time_point<chrono::steady_clock> start;
     unordered_set<Move> killerMoves[MAXIMUM_NEGAMAX_DEPTH + 1];
     int seconds;
     pair<Move, double> negamax(State& state, int depth);
-    double negamax(State& state, int currentDepth, int depth, double alpha, double beta, bool nullMove);
+    double negamax(State& state, int currentDepth, int depth, double alpha, double beta);
     double quiescenceSearch(State& state, int currentDepth, double alpha, double beta);
 public:
     static int getMaximumNegamaxDepth();
