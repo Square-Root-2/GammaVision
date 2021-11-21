635c225f6e59ccfa19697202b8f7131db591ece6
Gary Peng <22gapeng22@gmail.com>
2021-11-20 20:32:18 -0500

    11/20/2021

--- ChessEngine/Engine.cpp
+++ ChessEngine/Engine.cpp
@@ -35,8 +35,17 @@ double Engine::negamax(State& state, int currentDepth, int depth, double alpha, 
     vector<Move> moves = MoveGenerator::getMoves(state);
     if (moves.empty())
         return state.isActiveColorInCheck() ? -(Evaluator::getMaximumEvaluation() + getMaximumNegamaxDepth() + getMaximumQuiescenceDepth() + 1 - currentDepth) : 0;
-    if (currentDepth == depth)
+    if (currentDepth >= depth)
         return quiescenceSearch(state, currentDepth, alpha, beta);
+    if (!state.isActiveColorInCheck()) {
+        state.toggleActiveColor();
+        double evaluation = -negamax(state, currentDepth + 1, depth - R, -beta, -beta + 1);
+        if (evaluation == Evaluator::getMaximumEvaluation() + getMaximumNegamaxDepth() + getMaximumQuiescenceDepth() + 2)
+            return -(Evaluator::getMaximumEvaluation() + getMaximumNegamaxDepth() + getMaximumQuiescenceDepth() + 2);
+        state.toggleActiveColor();
+        if (evaluation >= beta)
+            return evaluation;
+    }
     mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
     shuffle(moves.begin(), moves.end(), rng);
     sort(moves.begin(), moves.end(), MoveComparator(killerMoves[currentDepth]));
