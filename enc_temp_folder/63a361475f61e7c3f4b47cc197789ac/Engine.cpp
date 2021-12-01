33ff36db6aa3601dff2dd1bfa057113c6ec33238
Gary Peng <22gapeng22@gmail.com>
2021-12-01 17:54:00 -0500

    12/1/2021

--- enc_temp_folder/63a361475f61e7c3f4b47cc197789ac/Engine.cpp
+++ enc_temp_folder/63a361475f61e7c3f4b47cc197789ac/Engine.cpp
@@ -4,13 +4,11 @@
 #include "MoveGenerator.h"
 #include "MoveType.h"
 #include <random>
-
+#include <iostream>
 pair<Move, int> Engine::negamax(State& state, int depth) {
     if (chrono::duration_cast<chrono::seconds>(chrono::steady_clock::now() - start).count() >= seconds)
         return pair<Move, int>(Move(0, 0, 0, 0, MoveType::TIMEOUT, ' ', ' '), 0);
     vector<Move> moves = MoveGenerator::getMoves(state); 
-    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-    shuffle(moves.begin(), moves.end(), rng);
     sort(moves.begin(), moves.end(), MoveComparator(killerMoves[0]));
     Move optimalMove;
     int alpha = INT32_MIN;
@@ -48,8 +46,6 @@ int Engine::negamax(State& state, int currentDepth, int depth, int alpha, int be
                 return quiescenceSearch(state, currentDepth, alpha, beta, false);
         }
     }
-    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-    shuffle(moves.begin(), moves.end(), rng);
     sort(moves.begin(), moves.end(), MoveComparator(killerMoves[currentDepth]));
     int optimalEvaluation = INT32_MIN;
     tuple<string, bool, int, int> hashCode = state.getHashCode();
@@ -80,8 +76,6 @@ int Engine::quiescenceSearch(State& state, int currentDepth, int alpha, int beta
     if (isQuietMove)
         return Evaluator::getEvaluation(state);
     int optimalEvaluation = INT32_MIN;
-    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-    shuffle(moves.begin(), moves.end(), rng);
     sort(moves.begin(), moves.end(), MoveComparator(killerMoves[0]));
     tuple<string, bool, int, int> hashCode = state.getHashCode();
     for (int i = 0; i < moves.size(); i++) {
