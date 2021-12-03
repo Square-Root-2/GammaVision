e4c0dd779f8d1194358c19ef3dfc191ed723a5da
Gary Peng <22gapeng22@gmail.com>
2021-12-02 23:47:08 -0500

    12/2/2021

--- ChessEngine/Engine.h
+++ ChessEngine/Engine.h
@@ -9,9 +9,9 @@ using namespace std;
 
 class Engine {
     static const int DR = 4, MAXIMUM_NEGAMAX_DEPTH = 30, MAXIMUM_QUIESCENCE_DEPTH = 30, MAX_R = 4, MIN_R = 3;
-    chrono::time_point<chrono::steady_clock> start;
     unordered_set<Move> killerMoves[MAXIMUM_NEGAMAX_DEPTH + 1];
     int seconds;
+    chrono::time_point<chrono::steady_clock> start;
     pair<Move, int> negamax(State& state, int depth);
     int negamax(State& state, int currentDepth, int depth, int alpha, int beta, bool nullOk);
     int quiescenceSearch(State& state, int currentDepth, int alpha, int beta);
